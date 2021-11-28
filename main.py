a5d4a8b71a0a7cf3b78dd749b52f7ab3061e52ac
CountVariable <46840113+IamWafula@users.noreply.github.com>
2021-11-28 12:18:04 +0300

    finall

--- main.py
+++ main.py
@@ -1,10 +1,57 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='static')
 
-@app.route("/")
+'''@app.route("/")
 def hello_world():
-    return "<p>Helloooo, World!</p>"
+    return render_template("main.html")
+'''
+
+def distance(rssi):
+    dist = []
+    rssi = abs(int(rssi))
+
+    if rssi>=100:
+        left = "80%"
+        top = str(980/rssi)
+        top = top+"%"
+    elif rssi >= 90:
+        left = "65%"
+        top = str(980/rssi)
+        top = top+"%"
+    elif rssi>= 80:
+        left = "40%"
+        top = str(980/rssi)
+        top = top+"%"
+    elif rssi >=70:
+        left = "35%"
+        top = str(980/rssi)
+        top = top+"%"
+    elif rssi >=60:
+        left = "30%"
+        top = str(980/rssi)
+        top = top+"%"
+    else:
+        left = "25%"
+        top = str(980/rssi)
+        top = top+"%"
+    
+    dist.append(left)
+    dist.append(top)
+
+    return dist
+
+
+
+
+@app.route("/<rssi>")
+def rssi(rssi):
+    dist = distance(rssi)
+
+    left = dist[0]
+    top = dist[1]
+
+    return render_template("main.html", left=left, top = top)
 
 
 app.run()
